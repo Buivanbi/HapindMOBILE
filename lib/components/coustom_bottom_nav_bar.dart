9cda26c9fee0743d530c1dfba50b91fe61b71de3
Hai lúa Tiểu <hailuatieu@Hai-lua-MacBook-Pro.local>
2023-12-05 18:23:21 +0700

    lua

---
+++ lib/components/coustom_bottom_nav_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:hapind/screens/chat/chats_screen.dart';
+import 'package:hapind/screens/home/home_screen.dart';
+import 'package:hapind/screens/profile/profile_screen.dart';
+
+import '../constants.dart';
+import '../enums.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({
+    Key? key,
+    required this.selectedMenu,
+  }) : super(key: key);
+
+  final MenuState selectedMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    final Color inActiveIconColor = Color.fromARGB(255, 10, 9, 9);
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 0.25),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 247, 247, 247),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: SafeArea(
+          top: false,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                // icon: SvgPicture.asset(
+                //   "assets/icons/Shop Icon.svg",
+                //   color: MenuState.home == selectedMenu
+                //       ? kPrimaryColor
+                //       : inActiveIconColor,
+                // ),
+                 icon: Icon(Icons.home,
+                  color: MenuState.home == selectedMenu
+                       ? kPrimaryColor
+                : inActiveIconColor,),
+                onPressed: () =>
+                    Navigator.pushNamed(context, HomeScreen.routeName),
+              ),
+              IconButton(
+                // icon: SvgPicture.asset("assets/icons/Heart Icon.svg"),
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.message),
+                onPressed: () =>Navigator.pushNamed(context,ChatsScreen.routeName),
+              ),
+              IconButton(
+                // icon: SvgPicture.asset(
+                //   "assets/icons/User Icon.svg",
+                //   color: MenuState.profile == selectedMenu
+                //       ? kPrimaryColor
+                //       : inActiveIconColor,
+                // ),
+                 icon: Icon(Icons.person,
+                 color: MenuState.profile == selectedMenu ? kPrimaryColor:inActiveIconColor
+                 ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, ProfileScreen.routeName),
+              ),
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hapind/components/coustom_bottom_nav_bar.dart';
+import 'package:hapind/enums.dart';
+import 'package:hapind/screens/home/components/slider.dart';
+
+//import 'components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = "/home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SliderHome(),
+      
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.home),
+      
+    );
+  }
+}
